92af67b1525399be271420ecc5340cd2659f07ad
bluespace <bluespace.dev@gmail.com>
2019-02-11 22:52:07 +0900

    Create 다익스트라_base_BOJ_1753.cpp

---
+++ Graph/Shortest_Path/다익스트라_base_BOJ_1753.cpp
@@ -0,0 +1,51 @@
+//SPFA - 최악의 경우 O(VE) 라 시간초과
+//다익스트라
+
+#include <cstdio>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define MAX_V   20000
+#define INF     1e9
+
+int V, E, K;
+vector<pair<int,int>> adj[MAX_V+1];
+
+int main(){
+    scanf("%d %d", &V, &E);
+    scanf("%d", &K);
+    while(E--){
+        int u,v,w;
+        scanf("%d %d %d",&u, &v, &w);
+        adj[u].push_back({w,v});
+    }
+    
+    vector<int> d(V+1, INF);
+
+    d[K] = 0;
+    priority_queue<pair<int,int>> pq;
+    pq.push({d[K], K});
+    while(!pq.empty()){
+        auto& a = pq.top();
+        int cost = -a.first;
+        int u = a.second;
+        pq.pop();
+        if(d[u] < cost)    continue;
+        for(auto& e : adj[u]){
+            int v = e.second;
+            int w = e.first;
+            if(d[v] > d[u] + w){
+                d[v] = d[u] + w;
+                pq.push({-d[v], v});
+            }
+        }
+    }
+    
+    for(int i = 1; i <= V; ++i)
+        if(d[i] >= INF) printf("INF\n");
+        else            printf("%d\n", d[i]);
+    
+    return 0;
+}
