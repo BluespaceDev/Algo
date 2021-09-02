cdc17fa2b3bef5449d6009e6851c3f87508124b0
bluespace <bluespace.dev@gmail.com>
2021-09-03 02:04:02 +0900

    Create 95. Unique Binary Search Trees II.cpp

---
+++ LeetCode/95. Unique Binary Search Trees II.cpp
@@ -0,0 +1,60 @@
+//https://leetcode.com/problems/unique-binary-search-trees-ii/
+// nullptr 을 생각하면 더 쉽게 풀 수 있다.
+// nullptr node 를 해서 코드를 
+
+class Solution {
+public:
+    vector<TreeNode*> generate(vector<int>& nums) {
+        vector<TreeNode*> ans;
+        
+        for (auto num1 : nums) {            
+            vector<int> left_nums, right_nums;
+            for (auto num2 : nums) {
+                if (num2 < num1)
+                    left_nums.push_back(num2);
+                else if (num2 > num1)
+                    right_nums.push_back(num2);
+            }
+            
+            auto left_child = generate(left_nums);
+            auto right_child = generate(right_nums);
+            
+            if (left_nums.size() && right_nums.size()) {
+                for (auto left_node : left_child) {
+                    for (auto right_node : right_child) {
+                        TreeNode* node = new TreeNode(num1);
+                        node->left = left_node;
+                        node->right = right_node;
+                        ans.push_back(node);
+                    }
+                }
+            }
+            else if (left_nums.size()) {
+                for (auto left_node : left_child) {
+                    TreeNode* node = new TreeNode(num1);
+                    node->left = left_node;
+                    ans.push_back(node);
+                }
+            }
+            else if (right_child.size()) {
+                for (auto right_node : right_child) {
+                    TreeNode* node = new TreeNode(num1);
+                    node->right = right_node;
+                    ans.push_back(node);
+                }
+            }
+            else {
+                ans.push_back(new TreeNode(num1));
+            }
+        }
+        
+        return ans;
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {
+        vector<int> nums;
+        for (int i = 1; i <= n; ++i)
+            nums.push_back(i);
+        return generate(nums);
+    }
+};
